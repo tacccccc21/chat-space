b403d28422cbccca2fceb1a571ae55cd986be147
YourName <you@example.com>
2020-03-11 01:42:01 +0900

    変更しました

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 Rails.application.routes.draw do
-  
-
   devise_for :users
-  get 'messages/index'
-
-  root "messages#index"
+  root 'groups#index'
+  resources :users, only: [:edit, :update]
+  resources :groups, only: [:new, :create, :edit, :update, :index, ]
 end
